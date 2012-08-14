0d0394fb338871665fec0d028b78ba89e32407f4
Kaare Hoff Skovgaard <khs@secoya.dk>
2012-08-14 10:27:13 +0200

    Aliased to global namespace

--- bootstrap.php
+++ bootstrap.php
@@ -6,3 +6,5 @@ Autoloader::add_classes(array(
 	'Secoya\\JSON\\JSON' => __DIR__.'/classes/json.php',
 	'Secoya\\JSON\\JSONException' => __DIR__.'/classes/json.php'
 ));
+
+Autoloader::alias_to_namespace('Secoya\\JSON\\JSON');
